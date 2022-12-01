69b6caada65727f86909620a663b478360cb7964
acmacomber <76853073+acmacomber@users.noreply.github.com>
2022-12-01 14:44:44 -0800

    HTML for presentation

--- vignette-group6/iforest.R
+++ vignette-group6/iforest.R
@@ -9,12 +9,12 @@ class <- credit %>% select(Class)
 credit <- credit %>% select(Time, Amount)
 
 #plot data
-ggplot(credit, aes(x = Time, y = Amount)) + 
+ggplot(credit, aes(x = Time, y = Amount)) +
   geom_point(shape = 1, alpha = 0.5) +
   labs(x = "Time", y = "Transaction Amount") +
-  labs(alpha = "", color="Legend")
+  labs(alpha = "", color = "Legend")
 
-# create isolation forest using isolationForest function from solitude package 
+# create isolation forest using isolationForest function from solitude package
 # with default parameters
 iforest <- isolationForest$new(num_trees = 1000)
 
@@ -22,10 +22,11 @@ iforest$fit(credit)
 
 #predict outliers within dataset
 credit$pred <- iforest$predict(credit)
-credit$outlier <- as.factor(ifelse(credit$pred$anomaly_score >=0.75, "outlier", "normal"))
+credit$outlier <-
+  as.factor(ifelse(credit$pred$anomaly_score >= 0.75, "outlier", "normal"))
 
 #plot data
-ggplot(credit, aes(x = Time, y = Amount, color = outlier)) + 
+ggplot(credit, aes(x = Time, y = Amount, color = outlier)) +
   geom_point(shape = 1, alpha = 0.5) +
   labs(x = "Time", y = "Transaction Amount") +
-  labs(alpha = "", color="Legend")
+  labs(alpha = "", color = "Legend")
